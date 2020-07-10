111aa17adc6f08bdf4a830b68a7fafef522b494b
YusukeHirose <kodecurry@gmail.com>
2020-07-11 01:30:52 +0900

    feat: api_keyを環境変数で受け取る

--- handler/movie.go
+++ handler/movie.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"fmt"
+	"mvindex/service"
 
 	"github.com/labstack/echo"
 )
 
 func GetMovies(c echo.Context) error {
 	fmt.Println("get movies")
+	service.GetTopRatedList()
 	return nil
 }
--- service/tmdb.go
+++ service/tmdb.go
@@ -2,24 +2,30 @@ package service
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
+	"mvindex/model"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
 const baseURL = "https://developers.themoviedb.org/3"
-const apiKey = "apiKey"
 
 func GetTopRatedList() {
-	url := baseURL + "/movie/top_rated?" + apiKey
-	req, _ := http.NewRequest("GET", url, nil)
-	client := new(http.Client)
-	res, err := client.Do(req)
+	var conf model.ApiConfig
+	err := envconfig.Process("", &conf)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
-	defer res.Body.Close()
-	byteArray, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(byteArray))
+	fmt.Println(conf.Key)
+	// url := baseURL + "/movie/top_rated?" + apiKey
+	// req, _ := http.NewRequest("GET", url, nil)
+	// client := new(http.Client)
+	// res, err := client.Do(req)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer res.Body.Close()
+	// byteArray, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println(string(byteArray))
 
 }
